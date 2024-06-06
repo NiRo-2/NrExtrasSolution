c09a9458d66f2eee2ab3dd9df378b7de62cc7dea
Ni Ro <nir.rozman@gmail.com>
2024-06-06 15:53:14 +0300

    -log info updates

---
+++ NrExtras.JsonHelper/JsonHelper.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NrExtras.JsonHelper
+{
+    public static class JsonHelper
+    {
+        /// <summary>
+        /// Validating json string
+        /// </summary>
+        /// <param name="jsonString"></param>
+        /// <returns>true if valid, false otherwise</returns>
+        public static bool IsValid(string jsonString)
+        {
+            try
+            {
+                JObject.Parse(jsonString);
+                return true;
+            }
+            catch (JsonReaderException)
+            {
+                return false;
+            }
+        }
+    }
+}
